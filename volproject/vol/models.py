30d5a1a54cfee629a35a776c165684b6095ca93e
Otto Jongerius <otto.jongerius@gmail.com>
2017-09-15 15:05:55 +1200

    Init of Django project for vol.

---
+++ volproject/vol/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+
+class Labels(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Organisation(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=70)
+    region = models.CharField(max_length=70)
+    city = models.CharField(max_length=70)
+    added = models.DateField('date added')
+
+    def __str__(self):
+        return self.name
+
+
+class Site(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    added = models.DateField('date added')
+
+    def __str__(self):
+        return self.added
+
+class Job(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=200)
+    labels = models.ManyToManyField(Labels)
+    organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE) # One org per job
+    site = models.ManyToManyField(Site)                                     # Possibly more sites per job
+    country = models.CharField(max_length=70) # TODO: move to country table?
+    region = models.CharField(max_length=70)  # TODO: move to region table?
+    city = models.CharField(max_length=70)    # TODO: move to city table?
+    added = models.DateField('date added')
+    seen = models.IntegerField(default=0)     # How often someone has looked at job
+
+    def __str__(self):
+        return self.text
