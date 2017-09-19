6f20205c452c5b519dc3c4e7e2b4e18fab5c04dd
Otto Jongerius <otto.jongerius@gmail.com>
2017-09-20 10:35:59 +1200

    Cleanup: remove unused code, outdated comments and logging statements.

--- volproject/vol/views.py
+++ volproject/vol/views.py
@@ -4,8 +4,6 @@ from django.db.models import Q
 from django.template import loader
 from django.shortcuts import render
 
-# Create your views here.
-
 def index(request):
     return HttpResponse(render(request, 'vol/index.html'))
 
