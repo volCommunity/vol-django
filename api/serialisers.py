81dafbccbdf2d6165ddf571598f2e299c8e8060c
Otto Jongerius <otto.jongerius@gmail.com>
2017-10-02 20:12:34 +1300

    Code reformatting.

--- api/serialisers.py
+++ api/serialisers.py
@@ -2,20 +2,24 @@ from vol.models import Labels, Organisation, Site, Job
 
 from rest_framework import serializers
 
+
 class LabelSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Labels
         fields = ['name']
+
 
 class OrganisationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Organisation
         fields = ('name', 'region', 'city', 'url', 'added')
 
+
 class SiteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Site
         fields = ('name', 'url', 'added')
+
 
 class JobSerializer(serializers.HyperlinkedModelSerializer):
     labels = LabelSerializer(read_only=True, many=True)
