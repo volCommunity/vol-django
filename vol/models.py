9c5fb0319980a6152b68ac4492410e3ba1af4ff6
Otto Jongerius <otto.jongerius@gmail.com>
2017-10-02 20:11:10 +1300

    Add data added to labels.

--- vol/models.py
+++ vol/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Labels(models.Model):
     name = models.CharField(max_length=200)
+    added = models.DateField('date added')
 
     def __str__(self):
         return self.name
