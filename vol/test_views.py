8d7fc6af5af5864207821e70400b97c69fd93fb4
Otto Jongerius <otto.jongerius@gmail.com>
2017-10-10 10:28:15 +1300

    Test secure redirect in web view, which is all for now.

--- vol/test_views.py
+++ vol/test_views.py
@@ -1,11 +1,13 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-# TODO: add same redirect test, and think of other things that make sense to test
+
 class IndexViewTests(TestCase):
-    # pass
+    # Assert we are redirected to a secure url
+    def test_index_insecure_redirects(self):
+        response = self.client.get(reverse('index'), secure=False)
+        self.assertEqual(response.status_code, 301)
+
     def test_index_view(self):
         response = self.client.get(reverse('index'), secure=True)
         self.assertEqual(response.status_code, 200)
-        # self.assertContains(response, "foo")
-        # self.assertQuerysetEqual(response.context['categories'], [])
