25859db2fa7bcd183556ad9a5983fbbf1f940ad1
Otto Jongerius <otto.jongerius@gmail.com>
2017-10-17 19:57:56 +1300

    Update factories to be dynamic.

--- vol/factories.py
+++ vol/factories.py
@@ -4,41 +4,45 @@ from .models import Job, Labels, Site, Organisation
 
 
 class LabelsFactory(factory.django.DjangoModelFactory):
-    name = "people"
+    name = factory.Sequence(lambda n: 'label {0}'.format(n))
 
     class Meta:
         model = Labels
+        django_get_or_create = ('name',)
 
 
 class SiteFactory(factory.django.DjangoModelFactory):
-    name = "Greenpeace"
-    url = "www.example.com/gp/jobs"
+    name = factory.Sequence(lambda n: 'Site {0}'.format(n))
+    url = factory.Sequence(lambda n: 'http://www.{0}.site'.format(n))
 
     class Meta:
         model = Site
+        django_get_or_create = ('name', 'url')
 
 
 class OrganisationFactory(factory.django.DjangoModelFactory):
-    name = "The nappy changers"
+    name = factory.Sequence(lambda n: 'Organisation {0}'.format(n))
     country = "NZ"
     city = "wellington"
     region = "wellington"
-    url = "www.example.com/organisations/nappychangers"
+    url = factory.Sequence(lambda n: 'http://www.{0}.org'.format(n))
 
     class Meta:
         model = Organisation
+        django_get_or_create = ('name', 'url')
 
 
 class JobFactory(factory.django.DjangoModelFactory):
-    title = "Nappy changer"
+    title = factory.Sequence(lambda n: 'Job title {0}'.format(n))
     text = "A create opportunity for a people person"
     country = "NZ"
     region = "wellington"
     city = "wellington"
-    url = "http://www.example.com/jobs/1"
+    url = factory.Sequence(lambda n: 'http://www.example.org/job/{0}'.format(n))
     seen = 0
 
     organisation = factory.SubFactory(OrganisationFactory)
 
     class Meta:
         model = Job
+        django_get_or_create = ('title', 'url')
