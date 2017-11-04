2e330111bd421ea8b0da719840d76dbc85976075
Otto Jongerius <otto.jongerius@gmail.com>
2017-11-04 23:28:45 +1300

    Add API documentation, including specifics about PUT behaviour.

--- api/views.py
+++ api/views.py
@@ -6,7 +6,20 @@ from .serialisers import JobSerializer, OrganisationSerializer, SiteSerializer, 
 
 class LabelViewSet(viewsets.ModelViewSet):
     """
-            API endpoint that allows Labels to be viewed or edited.
+    list:
+    API endpoint that returns a list of labels.
+
+    create:
+    API endpoint that creates a new label resource.
+
+    partial_update:
+    Not implemented.
+
+    update:
+    API endpoint that  updates an existing label resource.
+
+    delete:
+    API endpoint that deletes a label resource.
     """
     filter_fields = ('uuid', 'name', 'created_at', 'updated_at')
 
@@ -17,8 +30,20 @@ class LabelViewSet(viewsets.ModelViewSet):
 
 class OrganisationViewSet(viewsets.ModelViewSet):
     """
-        API endpoint that allows Organisations to be viewed or edited.
+    list:
+    API endpoint that returns a list of organisations.
 
+    create:
+    API endpoint that creates a new organisation resource.
+
+    partial_update:
+    Not implemented.
+
+    update:
+    API endpoint that  updates an existing organisation resource.
+
+    delete:
+    API endpoint that deletes a organisation resource.
     """
     filter_fields = ('uuid', 'name', 'description', 'country', 'region', 'city', 'url', 'created_at', 'updated_at')
 
@@ -29,7 +54,20 @@ class OrganisationViewSet(viewsets.ModelViewSet):
 
 class SiteViewSet(viewsets.ModelViewSet):
     """
-        API endpoint that allows Sites to be viewed or edited.
+    list:
+    API endpoint that returns a list of sites.
+
+    create:
+    API endpoint that creates a new site resource.
+
+    partial_update:
+    Not implemented.
+
+    update:
+    API endpoint that  updates an existing site resource.
+
+    delete:
+    API endpoint that deletes a site resource.
     """
     filter_fields = ('uuid', 'name', 'url', 'created_at', 'updated_at')
 
@@ -40,7 +78,30 @@ class SiteViewSet(viewsets.ModelViewSet):
 
 class JobViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows Jobs to be viewed or edited.
+    list:
+    API endpoint that returns a list of jobs.
+
+    create:
+    API endpoint that creates a new job resource.
+
+    partial_update:
+    Not implemented.
+
+    update:
+    API endpoint that  updates an existing job resource.
+
+    Note that the existing job will be updated, for nested resources the behaviour is as follows:\n
+    Organisation - If the organisation exists it will be linked, if it does not it will
+        be created. If the organisation included is invalid a validation error will be raised.\n
+    Labels - If an empty list is passed, current labels are removed from the
+        instance. If the list contains labels, a label will be created or if it exists
+        that resource will be linked. \n
+    Sites - If an empty list is passed, current labels are removed from the
+        instance. If the list contains labels, a label will be created or if it exists
+        that resource will be linked. If any of the sites is invalid a validation will be raised.
+
+    delete:
+    API endpoint that deletes a job resource.
     """
     filter_fields = ('uuid', 'title', 'text', 'labels', 'organisation', 'sites',
                      'country', 'region', 'city', 'created_at', 'updated_at',
