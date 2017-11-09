ceb7db2d52fa512f4809f4b3344b48c59b58fcf3
Otto Jongerius <otto@vol.community>
2017-11-09 21:38:34 +1300

    First stab at user login, includes Google, Facebook, Twitter and GH  logins. Allows users to login, logout or connect/disconnect open auth   providers.

--- vol/urls.py
+++ vol/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 from django.views.generic.base import RedirectView
 
 from . import views
@@ -9,5 +10,12 @@ urlpatterns = [
                                                permanent=False), name='favicon'),
     url('about', views.about, name='about'),
     url(r'^results/(?P<location>[\w\s+]+)/(?P<interests>[\w\s+]+)', views.results, name='results'),
-    url(r'^jobs/(?P<slug>[\d\w-]+)', views.job, name='job')  # TODO: should this be a nice name (SEO etc)
+    url(r'^jobs/(?P<slug>[\d\w-]+)', views.job, name='job'),  # TODO: should this be a nice name (SEO etc)
+
+    url(r'^login/?$', auth_views.login, name='login'),
+    url(r'^logout/?$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
+
+    url(r'^settings/?$', views.settings, name='settings'),
+    url(r'^settings/password/?$', views.password, name='password'),
 ]
