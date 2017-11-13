eec16d4a253fd193d6c81946fb7f15571d0b1b3d
Otto Jongerius <otto@vol.community>
2017-11-13 18:59:52 +1300

    Remove dead urls.

--- vol/urls.py
+++ vol/urls.py
@@ -15,7 +15,4 @@ urlpatterns = [
     url(r'^login/?$', auth_views.login, name='login'),
     url(r'^logout/?$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
-
-    url(r'^settings/?$', views.settings, name='settings'),
-    url(r'^settings/password/?$', views.password, name='password'),
 ]
