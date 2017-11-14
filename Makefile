20956a5754e1316ba7e927eae8a4c53dab9e4a22
Otto Jongerius <otto@vol.community>
2017-11-15 11:25:02 +1300

    Add user profile: * Profile model with one on one relationship with User * Changes to admin.py to allow manipulation using admin interface * Some extra commands in Makefile to make life using Docker more easy

--- Makefile
+++ Makefile
@@ -1,6 +1,15 @@
 build:
 	docker-compose build
 
+migrations:
+	docker-compose run web scripts/wait-for-it.sh db:5432 -- pipenv run python manage.py makemigrations
+	
+shell:
+	docker-compose run web scripts/wait-for-it.sh db:5432 -- pipenv run python manage.py shell
+
+migrate:
+	docker-compose run web scripts/wait-for-it.sh db:5432 -- pipenv run python manage.py migrate 
+
 test:
 	docker-compose run -e DEBUG="" web scripts/wait-for-it.sh db:5432 -- pipenv run python manage.py test
 
--- vol/models.py
+++ vol/models.py
@@ -1,6 +1,11 @@
 import uuid
-from django.db import models
+
 from autoslug import AutoSlugField
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Labels(models.Model):
@@ -56,7 +61,8 @@ class Job(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     labels = models.ManyToManyField(Labels)
-    organisation = models.ForeignKey(Organisation, on_delete=models.SET_NULL, null=True)  # Not all jobs have a known org
+    organisation = models.ForeignKey(Organisation, on_delete=models.SET_NULL,
+                                     null=True)  # Not all jobs have a known org
     sites = models.ManyToManyField(Site)  # Possibly more sites per job
     country = models.CharField(max_length=70)  # TODO: move to country table
     region = models.CharField(max_length=70, null=True)  # TODO: move to region table? TODO: don't use null for char
@@ -72,3 +78,20 @@ class Job(models.Model):
 
     class Meta:
         ordering = ['created_at']
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    favorite_jobs = models.ManyToManyField(Job, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    @receiver(post_save, sender=User)
+    def create_or_update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
